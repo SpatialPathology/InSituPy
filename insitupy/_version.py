a4a4e122cb7094211188d096ebdfc7e90d72c0ed
Johannes Wirth <j.wirth@tum.de>
2026-03-18 11:28:10 +0100

    build: read __version__ from importlib.metadata instead of hardcoded string

--- insitupy/_version.py
+++ insitupy/_version.py
@@ -1,3 +1,9 @@
-__version__ = "0.11.1"
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("insitupy")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 __author__ = "Johannes Wirth"
 __email__ = "j.wirth@tum.de"
